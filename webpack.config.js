ded0c3c3ef81fdb184d10e2179ef0f65d0ff5b9c
Ben Terrell <bterrell71@gmail.com>
2017-06-01 18:32:02 -0400

    added bootstrap
    dev build works. prod build does js not working

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,102 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var webpack = require('webpack');
-var path = require("path");
-//var tincan - require("tincan");
-//var myApp = new tincan("IM xAPI","778f48b68331df","a14a9bf93e57a3");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
+const path = require("path");
+const bootstrapEntryPoints = require('./webpack.bootstrap.config');
+const glob = require('glob');
+const PurifyCSSPlugin = require('purifycss-webpack');
 
-var isProd = process.env.NODE_ENV === 'production'; //true or false
-var cssDev = ['style-loader', 'css-loader', 'sass-loader'];
-var cssProd = ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          loader: ['css-loader','sass-loader'],
-          publicPath: './'
-        })
-var cssConfig = isProd ? cssProd : cssDev;
+const isProd = process.env.NODE_ENV === 'production'; //true or false
+const cssDev = [
+  'style-loader',
+  'css-loader?sourceMap',
+  'sass-loader',
+  {
+    loader: 'sass-resources-loader',
+    options: {
+      // Provide path to the file with resources
+      resources: [
+                './src/resources.scss'
+            ],
+    },
+  }];
+const cssProd = ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: ['css-loader','sass-loader', {
+    loader: 'sass-resources-loader',
+    options: {
+      // Provide path to the file with resources
+      resources: [
+        './src/resources.scss'
+      ],
+    },
+  }],
+    publicPath: '/dist'
+})
+const cssConfig = isProd ? cssProd : cssDev;
+
+const bootstrapConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
 
 module.exports = {
-  entry: {
-    app: './src/app.js',
-    contact: './src/contact.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: cssConfig
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        use: [
-          //'file-loader?name=images/[name].[ext]',
-          'file-loader?name=[name].[ext]&outputPath=images/',
-          'image-webpack-loader'
-          ]
-      }
-    ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    hot: true,
-    stats: "errors-only",
-    open: true
-  },
+    entry: {
+        app: './src/app.js',
+        bootstrap: bootstrapConfig
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: cssConfig
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                  'file-loader?name=images/[name].[ext]',
+                  'image-webpack-loader?bypassOnDebug'
+                ]
+            },
+            { test: /\.(woff2?)$/, use: 'url-loader?limit=10000&name=fonts/[name].[ext]' },
+            { test: /\.(ttf|eot)$/, use: 'file-loader?name=fonts/[name].[ext]' },
+            // Bootstrap 3
+            {
+                test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
+                use: 'imports-loader?jQuery=jquery'
+             }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        hot: true,
+        open: true,
+        stats: 'errors-only'
+    },
     plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Project Demo',
-        minify: {
-          collapseWhitespace: true
-        },
-        hash: true,
-        excludeChunks: ['contact'],
-        template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
-      }),
-      new HtmlWebpackPlugin({
-        title: 'Contact Page',
-        hash: true,
-        chunks: ['contact'],
-        filename: 'contact.html',
-        template: './src/contact.html'
-      }),
-      new ExtractTextPlugin({
-        filename: 'app.css',
-        disabled: !isProd,
-        allChunks: true
-      }),
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NamedModulesPlugin()
+        new HtmlWebpackPlugin({
+            title: 'Project Demo',
+            hash: true,
+            template: './src/index.html'
+        }),
+        new ExtractTextPlugin({
+            filename: 'css/[name].css',
+            disable: !isProd,
+            allChunks: true
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        // Make sure this is after ExtractTextPlugin!
+        new PurifyCSSPlugin({
+            // Give paths to parse for rules. These should be absolute!
+            paths: glob.sync(path.join(__dirname, 'src/*.html'))
+        })
     ]
 }
